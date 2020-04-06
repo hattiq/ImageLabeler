71709d4bd480745720a3bd6fcdb4c7dc6a00edee
HussainTaj-W <hussaintajwork@gmail.com>
2020-04-07 04:33:46 +0500

    Add Feature: Toggle Class Button off when next is pressed.

--- Ui_MainWindow.py
+++ Ui_MainWindow.py
@@ -217,6 +217,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
                         shutil.copyfile(source, destination)
                         self.fileCounter += 1
                         isCopied = True
+                        self.label_classes[label_btn].toggle()
                 if isCopied:
                     os.remove(source)
 
